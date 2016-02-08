e65d80922a0ff25ff7487784bead60c461de6dc4
Eric Vegors <tycobass@gmail.com>
2016-02-08 12:17:12 -0800

    version to put on branch

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -24,16 +24,6 @@ DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
 
-#title - unicode char to 255, unique, must be present
-#body - unicode any length including zero
-#create field - time of generation, default value is now
-#edited field - time of last edit, default value is now
-#class method of "all" that returns all the entries in the database, ordered so that the most recent entry is first
-#class method of "by_id" that returns a single entry, given an id
-#modified = Column(DateTime(timezone=True), default=datetime.datetime.utcnow)
-#     created = Column(DateTime(timezone=True), default = datetime.utcnow())  calll each time
-
-
 class Entry(Base):
     __tablename__ = 'entries'
     id = Column(Integer, primary_key=True)
@@ -46,33 +36,30 @@ class Entry(Base):
     @classmethod
     def all(cls, insight=None, session=None):
         #return the all entries sorted (desc) by id
-        if session == None:
+        if session == None:  # Assure session will work in pshell
             session = DBSession
-        q1 = session.query(cls)
-        all_entries = q1.order_by(desc(cls.id))
-        [print('\n', n.id, ' | ', n.title, ' | ', n.body, ' | ',\
-         n.created, ' | ', n.modified, '\n') for n in all_entries]
+        q1 = session.query(cls)   # alias "session.query"
+        all_entries = q1.order_by(desc(cls.id)) #create select all query
+        if insight != None:  # print results of query
+            cls.insight(all_entries)
         return all_entries
 
 
     @classmethod
-    def by_id(cls, id_num, session=None):
+    def by_id(cls, id_num, insight=None, session=None):
         #return the entry by the given id
-        if session == None:
+        if session == None:  # Assure session will work in pshell
             session = DBSession
-        q1 = DBSession.query(cls)
-        ### session.query(MyModel).filter(MyModel.value < 20)]
-        print('\nid_num value - ', id_num, '\n')
-        id_entry = q1.filter(cls.id==id_num)
-        [print('\n', n.id, ' | ', n.title, ' | ', n.body, ' | ',\
-         n.created, ' | ', n.modified, '\n', 'now method\n') for n in id_entry]
-        cls.insight(id_entry)
+        q1 = DBSession.query(cls)  # alias "session.query"
+        id_entry = q1.filter(cls.id==id_num)  #create select by id query
+        if insight != None:  # print results of query
+            cls.insight(id_entry)
         return id_entry
 
     @classmethod
     def insight(cls, query_result, session=None):
-        #return the all entries sorted (desc) by id
-        if session == None:
+        #Debug - print the query results for review by developer
+        if session == None:  # Assure session will work in pshell
             session = DBSession
         [print('\n', n.id, ' | ', n.title, ' | ', n.body, ' | ',\
          n.created, ' | ', n.modified, '\n') for n in query_result]
